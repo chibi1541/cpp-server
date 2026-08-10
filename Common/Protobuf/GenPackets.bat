fb2da4e1ab6afc89590e0ea589be9fce1347c4d9
BOOK-P5J347BQL9\user <runic1541@gmail.com>
2026-08-10 10:12:32 +0900

    Adjust GameServer Project Settings

--- Common/Protobuf/GenPackets.bat
+++ Common/Protobuf/GenPackets.bat
@@ -4,25 +4,25 @@ protoc.exe -I=./ --cpp_out=./ ./Enum.proto
 protoc.exe -I=./ --cpp_out=./ ./Struct.proto
 
 GenPackets.exe --path=./Protocol.proto --output=ClientPacketHandler --recv=C_ --send=S_
-GenPackets.exe --path=./Protocol.proto --output=ServerPacketHandler --recv=S_ --send=C_
+:: GenPackets.exe --path=./Protocol.proto --output=ServerPacketHandler --recv=S_ --send=C_
 
 IF ERRORLEVEL 1 PAUSE
 
-XCOPY /Y Enum.pb.h "../../GameServer"
-XCOPY /Y Enum.pb.cc "../../GameServer"
-XCOPY /Y Struct.pb.h "../../GameServer"
-XCOPY /Y Struct.pb.cc "../../GameServer"
-XCOPY /Y Protocol.pb.h "../../GameServer"
-XCOPY /Y Protocol.pb.cc "../../GameServer"
-XCOPY /Y ClientPacketHandler.h "../../GameServer"
+XCOPY /Y Enum.pb.h "../../GameServer" /E /Y /I
+XCOPY /Y Enum.pb.cc "../../GameServer" /E /Y /I
+XCOPY /Y Struct.pb.h "../../GameServer" /E /Y /I
+XCOPY /Y Struct.pb.cc "../../GameServer" /E /Y /I
+XCOPY /Y Protocol.pb.h "../../GameServer" /E /Y /I
+XCOPY /Y Protocol.pb.cc "../../GameServer" /E /Y /I
+XCOPY /Y ClientPacketHandler.h "../../GameServer" /E /Y /I
 
-XCOPY /Y Enum.pb.h "../../DummyClient"
-XCOPY /Y Enum.pb.cc "../../DummyClient"
-XCOPY /Y Struct.pb.h "../../DummyClient"
-XCOPY /Y Struct.pb.cc "../../DummyClient"
-XCOPY /Y Protocol.pb.h "../../DummyClient"
-XCOPY /Y Protocol.pb.cc "../../DummyClient"
-XCOPY /Y ServerPacketHandler.h "../../DummyClient"
+:: XCOPY /Y Enum.pb.h "../../DummyClient" /E /Y /I
+:: XCOPY /Y Enum.pb.cc "../../DummyClient" /E /Y /I
+:: XCOPY /Y Struct.pb.h "../../DummyClient" /E /Y /I
+:: XCOPY /Y Struct.pb.cc "../../DummyClient" /E /Y /I
+:: XCOPY /Y Protocol.pb.h "../../DummyClient" /E /Y /I
+:: XCOPY /Y Protocol.pb.cc "../../DummyClient" /E /Y /I
+:: XCOPY /Y ServerPacketHandler.h "../../DummyClient" /E /Y /I
 
 DEL /Q /F *.pb.h
 DEL /Q /F *.pb.cc
--- GameServer/pch.h
+++ GameServer/pch.h
@@ -1,4 +1,6 @@
-#pragma once
+﻿#pragma once
+
+#pragma warning(disable: 4251)
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
